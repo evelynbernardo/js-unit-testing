44dd233f9b1e875a1193bcd1d1788e4fa6e79052
evelynbernardo <bernardoevelynbarros@gmail.com>
2020-01-28 09:19:29 -0300

    Create 01.js

---
+++ nivel-0/01.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+var processamento = function (a) {
+    return a;
+}
+
+try {
+
+    var expected = 200;
+    var actual   = 200;
+
+    assert.equal(expected, processamento(actual));
+
+} catch(e) {
+    console.log(e);
+}
